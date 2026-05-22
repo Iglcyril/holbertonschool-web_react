2f873ae8c82e6effb6c9e7b231b442864091df85
Iglcyril <iglcyril@hotmail.fr>
2026-05-22 09:50:10 +0200

    add printTeacher function and its interface to main.ts

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
